8594206eaf10f50cd1a4c6095d08cd93b5f8d387
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-06-24 19:24:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import Details from './Details';
 import Play from './Play';
+import Dropdown from './Dropdown';
+//import Search from './Search';
 
 function App() {
 	return (
-		<div className='App'>
-			<div className='m-play'>
-				<audio></audio>
-				<h4>Playing Now</h4>
-				<Details />
-				<Play />
-				<p>
-					<strong>Next Up: Next Song</strong>
-				</p>
+		<>
+			<Dropdown />
+			<div className='App'>
+				<div className='m-play'>
+					<audio></audio>
+					<h4>Playing Now</h4>
+					<Details />
+					<Play />
+					<p>
+						<strong>Next Up: Next Song</strong>
+					</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
